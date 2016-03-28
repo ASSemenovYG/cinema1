15fc6561fd68bf2cc97c724d1e23daf541f80cce
artem199607 <artem-sem@mail.ru>
2016-03-28 15:48:27 +0300

    Update CinemaModel.java

--- src/main/java/CinemaModel.java
+++ src/main/java/CinemaModel.java
@@ -3,17 +3,73 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by IIS on 10.03.2016.
+ */
 public class CinemaModel {
+    public void addHall(int nrows, int seats) throws SQLException {
+        Connection connection = DConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO HALLS(NROWS,SEATS)" +
+                "VALUES(?,?)");
+        preparedStatement.setInt(1, nrows);
+        preparedStatement.setInt(2, seats);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+    }
 
-
-    List<Seance> find(String str) throws SQLException {
-        Connection c = DriverManager.getConnection("jdbc:derby:CinemaDB;create=true");
-        PreparedStatement find = c.prepareStatement("SELECT ID, STIME FROM SEANCES WHERE FILM=?");
-        find.setString(1,str);
-        ResultSet rs = find.executeQuery();
-        List<Seance> result = new ArrayList<Seance>();
-        while ()
+    public List<Halls> listHalls() throws SQLException {
+        List<Halls> list = new ArrayList<Halls>();
+        Connection connection = DConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT ID, NROWS, SEATS FROM HALLS");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            list.add(new Halls(resultSet.getInt(1), resultSet.getInt(2), resultSet.getInt(3)));
+        }
+        resultSet.close();
+        preparedStatement.close();
+        return list;
     }
 
 
+    List<Seances> find(String str) throws SQLException {
+        Connection c = DConnection.getConnection();
+        PreparedStatement find = c.prepareStatement("SELECT ID, STIME FROM SEANCES WHERE FILM=?");
+        find.setString(1, str);
+        ResultSet rs = find.executeQuery();
+        List<Seances> result = new ArrayList<Seances>();
+        while (rs.next()) {
+            int id = rs.getInt(1);
+
+            result.add(new Seances(0, null, null, 0, 0, 0));
+        }
+        return result;
+    }
+
+
+    public List<Seances> findSeance(Seances seances) throws SQLException {
+        Connection c = DConnection.getConnection();
+        PreparedStatement findSeance = c.prepareStatement();
+        List<Seances> list = new ArrayList<Seances>();
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT ID, STIME, AGE, PRICE, FILM, HALL FROM SEANCES WHERE 1=1 ");
+        if (seances.STIME != null){
+            query.append(" AND STIME >= ?");
+        }
+        if (seances.AGE != null){
+            query.append(" AND AGE >= ?");
+        }
+        if (seances.PRICE != null){
+            query.append(" AND PRICE >= ?");
+        }
+        if (seances.FILM != null){
+            query.append(" AND UPPER (FILM) LIKE '%" + film + "%' ");
+        }
+        if (hall != null){
+            query.append(" AND HALL = ?");
+        }
+
+        int row = 1;
+
+
+    }
 }
