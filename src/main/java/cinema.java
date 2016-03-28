60cc6b5eeac310ac4a94e365cff86ac9ea7732b5
artem199607 <artem-sem@mail.ru>
2016-03-28 15:46:11 +0300

    Update cinema.java

--- src/main/java/cinema.java
+++ src/main/java/cinema.java
@@ -1,17 +1,21 @@
+import javax.security.auth.login.CredentialException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
-
-public class cinema  {
-
-
-    private static Connection newDB;
-
-    public static void main(String args[]) throws SQLException {
-    createDB(newDB);
-
+/**
+ * Created by IIS on 25.02.2016.
+ */
+public class Cinema {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DConnection.getConnection();
+        CinemaModel cinemaModel = new CinemaModel();
+        cinemaModel.addHall(45, 5);
+        cinemaModel.addHall(4, 7);
+        List<Halls> list = cinemaModel.listHalls();
+        for (Halls halls : list) {
+            System.out.println(halls);
+        }
     }
 }
 
