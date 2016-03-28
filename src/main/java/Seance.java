65eb94508ede67ab49fa49bcdb30a51d12e9edbe
artem199607 <artem-sem@mail.ru>
2016-03-28 15:46:46 +0300

    Update and rename Seance.java to Seances.java

--- src/main/java/Seance.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by IIS on 10.03.2016.
- */
-public class Seance {
-    private String time;
-    private String name;
-}
---
+++ src/main/java/Seances.java
@@ -0,0 +1,17 @@
+public class Seances {
+    public final int ID;
+    public final String FILM;
+    public final String STIME;
+    public final int HALL;
+    public final int PRICE;
+    public final int AGE;
+
+    public Seances(int ID, String FILM, String STIME, int HALL, int PRICE, int AGE) {
+        this.ID = ID;
+        this.FILM = FILM;
+        this.STIME = STIME;
+        this.HALL = HALL;
+        this.PRICE = PRICE;
+        this.AGE = AGE;
+    }
+}
